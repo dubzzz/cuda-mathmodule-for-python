6a2d6d6d7e41867192ad69d01f40ee70e6ea64a1
Nicolas DUBIEN <github@dubien.org>
2014-06-04 19:11:10 +0100

    Add PyVector::__cinit__(np.ndarray[])
    Add some methods into mathmodule:
    * PyVector::__cinit__(np.ndarray[])
    * void PyVector::memsetZero()
    * unsigned int PyVector::size()

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Converting a NumPy array to a GPU-array:
 ```Python
 import numpy as np, mathmodule as mm
 numpy_array = np.random.random(5)
-gpu_array = mm.Vector(numpy_array)
+gpu_array = mm.PyVector(numpy_array)
 ```
 
 And GPU-array to NumPy array:
--- src/mathmodule.pyx
+++ src/mathmodule.pyx
@@ -11,8 +11,44 @@ cdef extern from "objects/Vector.hpp":
 
 cdef class PyVector:
     cdef Vector *thisptr
-    def __cinit__(self, unsigned int size):
-        self.thisptr = new Vector(size)
+    
+    def __cinit__(self, *args):
+        """
+        Init a Vector
+        1. From of a given size
+           WARNING: values in the array are not defined (memory-dependant)
+        2. From an existing NumPy array
+        """
+        
+        cdef np.ndarray ndarray
+        types = [type(arg) for arg in args]
+        if types == [int]:
+            self.thisptr = new Vector(args[0])
+        elif types == [np.ndarray]:
+            ndarray = args[0]
+            if ndarray.ndim == 1:
+                self.thisptr = new Vector(<double*> ndarray.data, ndarray.shape[0])
+            elif ndarray.ndim == 2 and ndarray.shape[1] == 1:
+                self.thisptr = new Vector(<double*> ndarray.data, ndarray.shape[0])
+            elif ndarray.ndim == 3 and ndarray.shape[1] == 1 and ndarray.shape[2] == 1:
+                self.thisptr = new Vector(<double*> ndarray.data, ndarray.shape[0])
+            else:
+                raise ValueError("PyVector::__cinit__\tMulti-dimensional arrays cannot be converted to PyVector objects")
+        else:
+            raise TypeError("PyVector::__cinit__\tUnknown method signature")
+
+    def __dealloc__(self):
+        del self.thisptr
+    
+    def memsetZero(self):
+        """ Reset array with zeros """
+        self.thisptr.memsetZero()
+    
+    def size(self):
+        """ Size of the array """
+        return self.thisptr.getSize()
+    
     def toNumPy(self):
+        """ NumPy equivalent of CUDA Vector """
         return self.thisptr.toNumPy()
 
