86a2093efb4c47d3dfe485dc950290159c5d7955
Nicolas DUBIEN <github@dubien.org>
2014-06-07 01:45:07 +0100

    Add options to benchmark procedure

--- Makefile
+++ Makefile
@@ -7,10 +7,11 @@ all: test
 rebuild: clean build
 
 test: build
-	python tests/benchmark.py 10000
+	python tests/benchmark.py 10000 1024 8192 65536
 	python tests/test.py
 
 build: clean
+	rm -f src/mathmodule.cpp
 	python setup.py build_ext --inplace $(CCFLAGS)
 
 clean:
--- tests/benchmark.py
+++ tests/benchmark.py
@@ -6,19 +6,7 @@ sys.path.append("../src/")
 sys.path.append("tests/../src/")
 
 num_tests = 100
-
-setup = """
-import numpy as np
-import mathmodule as mm
-
-height = 40960
-width = 40960
-
-a = np.random.random(width)
-b = np.random.random(width)
-va = mm.PyVector(a)
-vb = mm.PyVector(b)
-"""
+default_size = 4096
 
 if __name__ == '__main__':
     if len(sys.argv) >= 2:
@@ -27,11 +15,35 @@ if __name__ == '__main__':
         except ValueError:
             pass
     
-    print "PyVector::__iadd__"
-    print "> cuda: ", ti.timeit(stmt='va += vb', number=num_tests, setup=setup)
-    print "> numpy:", ti.timeit(stmt='a += b', number=num_tests, setup=setup)
+    try:
+        sizes = [int(s) for s in sys.argv[2:]]
+    except ValueError:
+        sizes = list()
+    
+    if len(sizes) == 0:
+        sizes.append(default_size)
+    
+    for size in sizes:
+        setup = """
+import numpy as np
+import mathmodule as mm
 
-    print "PyVector::__add__"
-    print "> cuda: ", ti.timeit(stmt='va + vb', number=num_tests, setup=setup)
-    print "> numpy:", ti.timeit(stmt='a + b', number=num_tests, setup=setup)
+height = %d
+width = %d
+
+a = np.random.random(width)
+b = np.random.random(width)
+va = mm.PyVector(a)
+vb = mm.PyVector(b)
+""" % (size, size)
+        
+        print "\nBENCHMARK -- #tests=", num_tests, "- size=", size, "\n"
+        
+        print "PyVector::__iadd__"
+        print "> cuda: ", ti.timeit(stmt='va += vb', number=num_tests, setup=setup)
+        print "> numpy:", ti.timeit(stmt='a += b', number=num_tests, setup=setup)
+
+        print "PyVector::__add__"
+        print "> cuda: ", ti.timeit(stmt='va + vb', number=num_tests, setup=setup)
+        print "> numpy:", ti.timeit(stmt='a + b', number=num_tests, setup=setup)
 
